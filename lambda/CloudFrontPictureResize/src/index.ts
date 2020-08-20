58f3b29516382da35fad1c3ec2f5765515bfd013
Stanislas Nanchen <240862+stanislas@users.noreply.github.com>
2020-08-20 13:47:47 +0200

    artificial change to publish lambda every time

--- lambda/CloudFrontPictureResize/src/index.ts
+++ lambda/CloudFrontPictureResize/src/index.ts
@@ -4,6 +4,7 @@ import * as resize from './resize';
 import {CloudFrontResponseEvent, Context} from 'aws-lambda';
 
 const s3 = new S3();
+const version = "0.1.4";
 
 exports.lambdaHandler = async (event: CloudFrontResponseEvent, _: Context) => {
     return await resize.handleEvent(s3, event)
