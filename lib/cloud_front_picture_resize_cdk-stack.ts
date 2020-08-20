8f0db11e18751aa810bf96fa1f24e21f879beda6
Stanislas Nanchen <240862+stanislas@users.noreply.github.com>
2020-08-20 13:31:35 +0200

    Add a SSM Parameter as output for the function version name.

--- lib/cloud_front_picture_resize_cdk-stack.ts
+++ lib/cloud_front_picture_resize_cdk-stack.ts
@@ -3,13 +3,21 @@ import {Duration} from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as iam from '@aws-cdk/aws-iam';
 import * as s3 from '@aws-cdk/aws-s3';
+import * as ssm from '@aws-cdk/aws-ssm';
 import * as path from 'path';
+import * as customresources from '@aws-cdk/custom-resources';
+import {AwsCustomResourcePolicy, PhysicalResourceId} from "@aws-cdk/custom-resources";
 
 export interface StackProps {
     staticS3BucketName: string
 }
 
+export const ResizeFunctionVersionArn = "ResizeFunctionVersionArn"
+
 export class CloudFrontPictureResizeCdkStack extends cdk.Stack {
+
+    public readonly resizeFunctionVersionArnParameter: string
+
     constructor(scope: cdk.App, id: string, stackProps: StackProps, cdkProps?: cdk.StackProps) {
         super(scope, id, cdkProps);
 
@@ -48,9 +56,39 @@ export class CloudFrontPictureResizeCdkStack extends cdk.Stack {
         })
 
         const version = resizeLambda.currentVersion;
-        new cdk.CfnOutput(this, "ResizeFunction", {
-            value: version.functionArn
-        })
 
+        this.resizeFunctionVersionArnParameter = id + ResizeFunctionVersionArn
+
+        const parameter = new ssm.StringParameter(this, ResizeFunctionVersionArn, {
+            parameterName: this.resizeFunctionVersionArnParameter,
+            stringValue: version.functionArn
+        })
     }
+
+    resizeFunctionVersionArnFromParameter(scope: cdk.Construct, id: string) {
+        return this.readParameterCustomResource(scope, id, this.resizeFunctionVersionArnParameter)
+    }
+
+    readParameterCustomResource(scope: cdk.Construct, id: string, parameterName: string) {
+        return new customresources.AwsCustomResource(scope, id, {
+            onUpdate: {
+                service: 'SSM',
+                action: 'getParameter',
+                parameters: {
+                    Name: parameterName
+                },
+                region: this.region,
+                physicalResourceId: PhysicalResourceId.of(id + Date.now().toString())
+            },
+            policy: AwsCustomResourcePolicy.fromSdkCalls({
+                resources: [this.formatArn({
+                    service: 'ssm',
+                    resource: 'parameter',
+                    sep: '',
+                    resourceName: parameterName
+                })]
+            })
+        }).getResponseField("Parameter.Value")
+    }
+
 }
--- test/cloud_front_picture_resize_cdk.test.ts
+++ test/cloud_front_picture_resize_cdk.test.ts
@@ -2,12 +2,17 @@ import {expect as expectCDK, matchTemplate, MatchStyle, haveResource} from '@aws
 import * as cdk from '@aws-cdk/core';
 import * as CloudFrontPictureResizeCdk from '../lib/cloud_front_picture_resize_cdk-stack';
 
+const TestStackName = "TestStack"
+
 test('Lambda Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new CloudFrontPictureResizeCdk.CloudFrontPictureResizeCdkStack(app, 'MyTestStack', {staticS3BucketName: "staticS3Bucket"});
+    const stack = new CloudFrontPictureResizeCdk.CloudFrontPictureResizeCdkStack(app, TestStackName, {staticS3BucketName: "staticS3Bucket"});
     // THEN
     expectCDK(stack).to(haveResource("AWS::Lambda::Function", {
         Timeout: 30
+    }))
+    expectCDK(stack).to(haveResource("AWS::SSM::Parameter", {
+        Name: TestStackName + CloudFrontPictureResizeCdk.ResizeFunctionVersionArn
     }))
 });
